3814b5e9dbbab7794f6bddae5dc2245692b16ee9
Xin Long He <xinlong-@hotmail.com>
2026-07-03 05:50:11 +0800

    Project Init

--- packages/ui/src/components/sidebar/sidebar.tsx
+++ packages/ui/src/components/sidebar/sidebar.tsx
@@ -58,7 +58,6 @@ function SidebarProvider({
   open: openProp,
   onOpenChange: setOpenProp,
   className,
-  style,
   children,
   ...props
 }: React.ComponentProps<"div"> & {
@@ -130,15 +129,10 @@ function SidebarProvider({
     <SidebarContext.Provider value={contextValue}>
       <div
         data-slot="sidebar-wrapper"
-        style={
-          {
-            "--sidebar-width": SIDEBAR_WIDTH,
-            "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
-            ...style,
-          } as React.CSSProperties
-        }
+
         className={cn(
           "group/sidebar-wrapper flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
+          `[--sidebar-width:${SIDEBAR_WIDTH}] [--sidebar-width-icon:${SIDEBAR_WIDTH_ICON}]`,
           className
         )}
         {...props}
